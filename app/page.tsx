49a0bc8377e7a2cea6e4e5f95098adf76ce9adf3
rivaldojun <adikpetojun@gmail.com>
2025-04-11 01:27:40 +0200

    dd

--- app/page.tsx
+++ app/page.tsx
@@ -355,7 +355,7 @@ export default function MesDashboard() {
           </div>
 
           {/* Ateliers Grid */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-fit mx-auto">
             {ateliers.map((atelier) => (
               <Link href={`/ateliers/${atelier.id}`} key={atelier.id} className="group">
                 <motion.div whileHover={{ y: -5 }} transition={{ type: "spring", stiffness: 300 }}>
